b6e440c38b17a42d7cc17860aebf00f599d12114
jacquemar <arnaud.timite12@gmail.com>
2023-03-06 16:50:00 +0000

    affichage cassé

---
+++ src/datas/ProductsList.jsx
@@ -0,0 +1,27 @@
+import monstera from "../assets/products/MOJO.jpg";
+export const productsList = [
+  {
+    name: "JOKER",
+    category: "poster",
+    id: "1ed",
+    available: true,
+    prix: "3.000 XOF",
+    cover: monstera,
+  },
+  {
+    name: "DOMO",
+    category: "poster",
+    id: "1ed",
+    disponible: true,
+    prix: "3.000 XOF",
+    cover: monstera,
+  },
+  {
+    name: "SPICY",
+    category: "poster",
+    id: "1ed",
+    disponible: true,
+    prix: "3.000 XOF",
+    cover: monstera,
+  },
+];
